691c11b65a490c6228039a77d5416b248c264336
David Arno <david@davidarno.org>
2022-09-15 08:41:00 +0100

    Trying a new layout to work with the i75 board

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setup(
         '': 'src'
     },
     py_modules = [
-        "i75_display_driver/lib/JPEGdecoder",
-        "i75_display_driver/lib/PNGdecoder"
+        "3rd_party/JPEGdecoder",
+        "3rd_party/PNGdecoder"
     ],
     long_description=open('README.md').read()
 )
