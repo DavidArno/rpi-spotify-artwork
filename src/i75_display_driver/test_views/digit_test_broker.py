5346d7eea5798c2b68cbbffc7740cf626113cbaa
David Arno <david@davidarno.org>
2022-09-26 16:58:38 +0100

    expanded functionality of digit_test_broker

--- src/i75_display_driver/test_views/digit_test_broker.py
+++ src/i75_display_driver/test_views/digit_test_broker.py
@@ -14,19 +14,20 @@ class DigitTestBroker():
     def __init__(self, display:Display):
         self._display = display
 
-    def handle_digit_test_message(self, digit:MessageBody) -> bool:
-        print("hdtm1")
-        a = int(digit)
-        print("hdtm2")
-        b = RGBColour(0x400000)
-        print("hdtm3")
-        c = RGBColour(0xFFFFFF)
-        print("hdtm4")
-        d = rgb_colour_to_hub75_colour(b)
-        print("hdtm5")
-        e = rgb_colour_to_hub75_colour(c)
-        print("hdtm6")
-        sprite = get_coloured_digit_image_data(a, d, e)
+    def handle_digit_test_message(self, data:MessageBody) -> bool:
+        parts = data.split("-")
+        print("hdtm1")  
+        digit = RGBColour(int(parts[0]))
+        back_colour = RGBColour(int(parts[1], 16)) if len(parts) >= 2 else RGBColour(0x400000)
+        fore_colour = RGBColour(int(parts[2], 16)) if len(parts) >= 3 else RGBColour(0xFFFFFF)
+        print("hdtm2")  
+
+        sprite = get_coloured_digit_image_data(
+            digit,
+            rgb_colour_to_hub75_colour(back_colour),
+            rgb_colour_to_hub75_colour(fore_colour)
+        )
+
         print("hdtm7")        
         self._display.draw_sprite(2, 2, sprite, layer = Layer.Bottom)
         self._display.render_display()
